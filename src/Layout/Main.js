b82ae4e2ddfe4ef60576a0ab879c0f699ef775b1
Rubayed Ahmed <rubayedahmed04@gmail.com>
2023-03-26 11:13:41 +0600

    Banner design complete

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+import Header from "../Pages/Shared/Header/Header";
+import Banner from "../Pages/Home/Banner/Banner";
+
+const Main = () => {
+
+
+  return (
+    <div>
+        <Header />
+        <Banner />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            {/* <h1 className="text-5xl text-center">Hi </h1> */}
+        </div>
+    );
+};
+
+export default Header;

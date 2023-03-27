544e8117bee3fbae9cd79c54d8c6fabe3352f6f6
Rubayed Ahmed <rubayedahmed04@gmail.com>
2023-03-27 12:58:22 +0600

    experience section added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -2,15 +2,16 @@ import React, {} from "react";
 import Header from "../Pages/Shared/Header/Header";
 import Banner from "../Pages/Home/Banner/Banner";
 import About from "../Pages/Home/About/About";
+import Experience from "../Pages/Home/Experience/Experience";
 
 const Main = () => {
-
 
   return (
     <div>
         <Header />
         <Banner />
         <About/>
+        <Experience/>
     </div>
   );
 };
--- src/Pages/Home/Experience/Experience.js
+++ src/Pages/Home/Experience/Experience.js
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaCheckCircle } from "react-icons/fa";
 
 const Experience = () => {
+  const [experience, setExperience] = useState();
+  const [exBackend, setExBackend] = useState();
+
+  useEffect(() => {
+    fetch("experience.json")
+      .then((res) => res.json())
+      .then((data) => setExperience(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("exBackend.json")
+      .then((res) => res.json())
+      .then((data) => setExBackend(data));
+  }, []);
+
   return (
-    <section>
+    <section className="px-10 md:px-20 lg:px-40 md:py-12 my-20">
       <div className="text-center">
-        <h6 className="capitalize text-sm">Get to know</h6>
+        <h6 className="capitalize text-sm">What skills i have</h6>
         <h3 className="text-3xl text-rose-500 font-bold mt-3 capitalize">
           Experience
         </h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center items-center mt-20">
+        <div className="border border-gray-400 h-96 rounded-3xl text-center">
+          <h3 className="text-rose-500 font-bold my-6 text-xl">
+            Frontend Development
+          </h3>
+          <ul className="px-12 grid grid-cols-2">
+            {experience?.map((ex) => (
+            <li className="flex text-left gap-6 justify-start my-4">
+            <span className="text-rose-500 text-lg">
+              <FaCheckCircle />
+            </span>
+            <div>
+              <h4 className="text-lg">{ex?.skill}</h4>
+              <small className="text-sm text-slate-400">{ex?.level}</small>
+            </div>
+          </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="border border-gray-400 h-96 rounded-3xl text-center">
+          <h3 className="text-rose-500 font-bold my-6 text-xl">
+            Backend Development
+          </h3>
+          <ul className="px-12 grid grid-cols-2">
+            {exBackend?.map((ex) => (
+            <li className="flex text-left gap-6 justify-start my-4">
+            <span className="text-rose-500 text-lg">
+              <FaCheckCircle />
+            </span>
+            <div>
+              <h4 className="text-lg">{ex?.skill}</h4>
+              <small className="text-sm text-slate-400">{ex?.level}</small>
+            </div>
+          </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );

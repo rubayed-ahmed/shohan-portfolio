8fa5a5b0107b0ae28c2ffd7f2a7834009cc71028
Rubayed Ahmed <rubayedahmed04@gmail.com>
2023-03-27 12:10:46 +0600

    about title updated

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { FaAward, FaUsers, FaFolderOpen } from "react-icons/fa";
 import me from "../../../assets/me.jpg";
 
 const About = () => {
+  useEffect(() => {
+    let style1 = document.createElement("style")
+    let style2 = document.createElement("style")
+    let after = document.getElementById("after-te1")
+    let before = document.getElementById("before-te1")
+    const setKeyframesRules = (n, start = 0) => {
+      let steps = ""
+      for (let i = start; i <= n; i++) {
+        let percent = (i / n) * 100
+        let random1 = `${Math.random()*150}px`
+        let random2 = `${Math.random()*150}px`
+        steps = steps.concat(`${percent}% { clip: rect(${random1}, 9999px, ${random2}, 0) } `)
+      }
+      return steps
+    }
+    let keyframes1 = `@keyframes glitch-anim-1 { ${setKeyframesRules(24)} }`
+    let keyframes2 = `@keyframes glitch-anim-2 { ${setKeyframesRules(32, 2)} }`
+    style1.innerHTML = keyframes1
+    style2.innerHTML = keyframes2
+    after.appendChild(style1)
+    before.appendChild(style2)
+    after.style.animation = "glitch-anim-1 2.5s infinite linear alternate-reverse"
+    before.style.animation = "glitch-anim-2 3s infinite linear alternate-reverse"
+  }, [])
   return (
     <section id="about" className="px-10 md:px-20 lg:px-40 md:py-12 my-20">
-      <div className="text-center">
-        <h6 className="capitalize text-sm">Get to know</h6>
-        <h3 className="text-3xl text-rose-500 font-bold mt-3 capitalize">
-          About me
-        </h3>
-      </div>
+    <div className="h-full flex justify-center items-center">
+      <h1 className="text-white text-4xl font-bold uppercase relative inline-block">
+        <span id="before-te1" className="absolute top-0 left-0.5 w-full h-full bg-transparent" 
+        style={{ textShadow: "-2px 0 #F43F5E", clipPath: "rect(24px, 550px, 90px, 0)" }} aria-hidden="true">About Me</span> {/* glitch::before */}
+        About Me
+        <span id="after-te1" className="absolute top-0 -left-0.5 w-full h-full bg-transparent" 
+        style={{ textShadow: "-2px 0 spin(#F43F5E, 180)", clipPath: "rect(85px, 550px, 140px, 0)" }} aria-hidden="true">About Me</span> {/* glitch::after */}
+      </h1>
+    </div>
       <div className="grid grid-cols-2 gap-6 justify-center items-center mt-20">
         <div className="">
           <div className="w-[70%] h-96 rounded-3xl bg-gradient-to-r from-transparent to-rose-500 aspect-square">
---
+++ src/Pages/Home/Experience/Experience.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <section>
+      <div className="text-center">
+        <h6 className="capitalize text-sm">Get to know</h6>
+        <h3 className="text-3xl text-rose-500 font-bold mt-3 capitalize">
+          Experience
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;

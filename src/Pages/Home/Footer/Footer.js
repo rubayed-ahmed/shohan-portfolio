b5ae28385b94b6f9a585cd0cee9c463243a1bd60
Rubayed Ahmed <rubayedahmed04@gmail.com>
2023-04-01 15:09:26 +0600

    Footer redesign

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -5,12 +5,12 @@ const Footer = () => {
     return (
         <footer className="px-10 md:px-20 lg:px-40 md:py-24 text-center bg-rose-500">
             <h3 className="text-3xl">Rubayed Ahmed</h3>
-            <ul className="flex items-center justify-center my-6 gap-12">
-                <li>Home</li>
-                <li>About</li>
-                <li>Experience</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
+            <ul className="flex items-center justify-center my-12 gap-12">
+                <a href="/#home" className="cursor-pointer">Home</a>
+                <a href="/#about" className="cursor-pointer">About</a>
+                <a href="/#experience" className="cursor-pointer">Experience</a>
+                <a href="/#works" className="cursor-pointer">Portfolio</a>
+                <a href="/#contact" className="cursor-pointer">Contact</a>
             </ul>
             <div className="flex justify-center gap-4 mt-12 mb-5">
                 <span className="bg-white text-rose-500 p-3 rounded-lg text-xl"><FaFacebook/></span>

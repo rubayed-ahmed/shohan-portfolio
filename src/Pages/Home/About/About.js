e666eedab3c2b938c408c810bc26329e480a4db8
Rubayed <rubayedahmed04@gmail.com>
2023-04-27 11:36:49 +0600

    About section complete

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,16 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { FaAward, FaUsers, FaFolderOpen } from "react-icons/fa";
-import CountUp from "react-countup";
-import ScrollTrigger from "react-scroll-trigger";
+import React, { useEffect } from "react";
+import {Link} from "react-router-dom"
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const About = () => {
-  const [countOn, setCountOn] = useState(false);
-
   useEffect(() => {
     AOS.init();
-  }, [])
+  }, []);
 
   return (
     <section id="about" className="px-10 md:px-20 lg:px-40 md:py-12 my-20">
@@ -20,11 +16,12 @@ const About = () => {
           About me
         </h3>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center items-center mt-20" >
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center items-center mt-20">
         <div className="mb-12 md:mb-0" data-aos="fade-right">
           <div
             className="w-[70%] h-96 rounded-3xl bg-gradient-to-r from-transparent mx-auto
-          to-rose-500 aspect-square">
+          to-rose-500 aspect-square"
+          >
             <img
               src="https://i.ibb.co/HHC9rWw/341d877a7c26db6c341d877a7c26db6cpersonal.jpg"
               className="w-full h-96 object-cover rounded-3xl rotate-[10deg] hover:rotate-0 overflow-hidden
@@ -35,86 +32,24 @@ const About = () => {
         </div>
 
         <div data-aos="fade-left">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-center text-center">
-            <div className="bg-rose-500 py-8 rounded-2xl">
-              <ScrollTrigger
-                onEnter={() => setCountOn(true)}
-                onExit={() => setCountOn(false)}
-              >
-                <div>
-                  <FaAward className="w-full text-3xl mb-3" />
-                  <span className="text-lg capitalize">Experience</span>
-                  <h1 className="text-lg mt-1">
-                    {countOn && (
-                      <CountUp
-                        start={0}
-                        end={3}
-                        duration={2}
-                        delay={0}
-                      ></CountUp>
-                    )}
-                    years+
-                  </h1>
-                </div>
-              </ScrollTrigger>
-            </div>
-
-            <div className="bg-rose-500 py-8 rounded-2xl">
-              <ScrollTrigger
-                onEnter={() => setCountOn(true)}
-                onExit={() => setCountOn(false)}
-              >
-                <div>
-                  <FaUsers className="w-full text-3xl" />
-                  <span className="text-lg capitalize">Clients</span>
-                  <h1 className="text-lg mt-1">
-                    {countOn && (
-                      <CountUp
-                        start={0}
-                        end={220}
-                        duration={2}
-                        delay={0}
-                      ></CountUp>
-                    )}
-                    +
-                  </h1>
-                </div>
-              </ScrollTrigger>
-            </div>
-
-            <div className="bg-rose-500 py-8 rounded-2xl">
-              <ScrollTrigger
-                onEnter={() => setCountOn(true)}
-                onExit={() => setCountOn(false)}
-              >
-                <div>
-                  <FaFolderOpen className="w-full text-3xl" />
-                  <span className="text-lg capitalize">Projects</span>
-                  <h1 className="text-lg mt-1">
-                    {countOn && (
-                      <CountUp
-                        start={0}
-                        end={18}
-                        duration={2}
-                        delay={0}
-                      ></CountUp>
-                    )}
-                    +
-                  </h1>
-                </div>
-              </ScrollTrigger>
-            </div>
+          <div>
+            <h3 className="text-2xl font-semibold">Get to know</h3>
+            <p className="mt-6 mb-12 text-slate-300">
+              I'm a <strong>FullStack Web Developer </strong> building the Front-end and Backend of Websites
+              and Web Applications that leads to the success of the overall
+              product. Check out some of my work in the <strong>Projects</strong> section.
+              <br /> <br/>
+              I also like sharing content related to the stuff that I have
+              learned over the years in <strong>Web Development</strong> so it can help other
+              people of the Dev Community. Feel free to Connect or Follow me on
+              my <Link tp="" className="text-rose-500 font-bold underline">Linkedin</Link> where I post useful content related to Web Development
+              and Programming
+              <br /> <br/>
+              I'm open to <strong>Job</strong> opportunities where I can contribute, learn and
+              grow. If you have a good opportunity that matches my skills and
+              experience then don't hesitate to <strong>contact</strong> me.
+            </p>
           </div>
-          <p className="mt-6 mb-12 text-slate-300">
-            A web developer is a professional who is responsible for designing,
-            building, and maintaining websites. They use a variety of
-            programming languages, such as HTML, CSS, and JavaScript, to create
-            websites that are both functional and visually appealing.
-            <br /> <br /> Web developers work on projects that range from simple
-            websites to complex web applications. They collaborate with clients
-            and other team members to understand their needs and goals, and to
-            ensure that the website or application meets those requirements.
-          </p>
           <a
             className="bg-rose-500 hover:bg-transparent border border-rose-500 transition-all 
               duration-300 ease-linear py-2 px-6 rounded"
